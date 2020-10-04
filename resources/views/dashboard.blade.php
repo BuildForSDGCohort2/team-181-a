3197408fd2da2a201f5a319fd2c2d68bdd684677
jimkiarie8@gmail.com <jimmy@localhost.localdomain>
2020-10-04 13:44:11 +0300

    commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -79,8 +79,8 @@
       <div class="row">
         <div class="col-md-4">
           {{-- this chart eill display :
-            Daily dairy sales for the farmer , 
-            Monlthly Births for the vet, 
+            Daily dairy sales for the farmer ,
+            Monlthly Births for the vet,
             Suggest something for the Field extention oficer,
             And maybe the daily orders from farmers a supplier has recieved
               --}}
@@ -197,7 +197,7 @@
                               <i class="material-icons">visibility</i>
                             </button>
                           </td>
-                        </tr>  
+                        </tr>
                       @endif
                       @empty
                       <tr>
@@ -220,7 +220,7 @@
                             <i class="material-icons">close</i>
                           </button>
                         </td>
-                      </tr>                          
+                      </tr>
                       @endforelse
                     </tbody>
                   </table>
@@ -242,7 +242,7 @@
                               <i class="material-icons">visibility</i>
                             </button>
                           </td>
-                        </tr>  
+                        </tr>
                       @endif
                       @empty
                       <tr>
@@ -265,7 +265,7 @@
                             <i class="material-icons">close</i>
                           </button>
                         </td>
-                      </tr>                          
+                      </tr>
                       @endforelse
                     </tbody>
                   </table>
@@ -487,7 +487,7 @@
                   </table>
                 </div>
               </div>
-            </div>          
+            </div>
           </div>
         </div>
       </div>
@@ -496,10 +496,23 @@
 @endsection
 
 @push('js')
-  <script>
+<script>
+    var chart_1_label = ['M', 'T', 'W', 'T', 'F', 'S', 'S']
+    var chart_1_series = [
+        [12, 17, 7, 17, 23, 18, 38]
+        ]
+
+    var chart_2_label = ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D']
+    var chart_2_series = [
+          [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
+        ]
+    var chart_3_label = ['12p', '3p', '6p', '9p', '12p', '3a', '6a', '9a']
+    var chart_3_series = [
+          [230, 750, 450, 300, 280, 240, 200, 190]
+        ]
     $(document).ready(function() {
       // Javascript method's body can be found in assets/js/demos.js
-      md.initDashboardPageCharts();
+      md.initDashboardPageCharts(chart_1_label,chart_1_series,chart_2_label,chart_2_series,chart_3_label,chart_3_series);
     });
-  </script>
+</script>
 @endpush
